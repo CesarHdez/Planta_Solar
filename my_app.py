e9492f75f63de2f687ad810c4cc838e603e8a938
CesarHdez <chdez293@gmail.com>
2020-12-29 23:56:08 -0300

    app front update

--- my_app.py
+++ my_app.py
@@ -13,46 +13,39 @@ from tensorflow.keras.models import Sequential
 
 os.environ['TZ'] = 'UTC'
 
-flag_input_show = False
-
-# model=Sequential()
-# data=pd.DataFrame()
-# data_r= np.empty(24,)
-# data_mean_2=[]
-# data_std_2=[]
-
 @st.cache
-def load_data():
-	data = pd.read_excel('full_data.xlsx', sheet_name='data')
+def load_data(data=None):
+	if data is None:
+		data = pd.read_excel('full_data.xlsx', sheet_name='data')
+	else:
+		data = pd.read_excel(data, sheet_name='data')
 	data['DateTime'] = pd.to_datetime(data['DateTime'])
 	data.set_index('DateTime', inplace=True)
 	data = data.astype(float)
-	data = data[:200]
+	data = data[:300]
 	return data
 	
-# @st.cache
 def load_models():
 	list_config, list_model = app_tools.get_lib_models()
 	return list_config, list_model
 
 #stt.set_theme({'primary': '#ffffff'})
 st.title('Predictor de Producción Fotovoltaica')
+st.image("solar_plant.jpg")
 
-load_state = st.text('Cargando datos y modelos...')
+load_state = st.empty()
+load_state.text("")
 
-data = load_data()
-data_master = data
-list_config, list_model = load_models()
-
-load_state = st.text('Carga completada!')
-
+st.sidebar.header('Datos')
+upload_file = st.sidebar.file_uploader("Cargue el archivo de datos (.csv o .xlsx)", type=["csv","xlsx"])
+#--------------------------------------------------------------------------
 st.sidebar.header('Parámetros del Modelo')
 def model_parameters():
-    neurons = st.sidebar.selectbox('Número de Neuronas',(50,100,150,200))
-    act_func = st.sidebar.selectbox('Función de Activación',('relu','elu','selu','LeakyReLU'))
-    loss = st.sidebar.selectbox('Función de Pérdida',('mse','mae','huber_loss','logcosh'))
-    optmz = st.sidebar.selectbox('Optimizador',('Adam','RMSprop','Adagrad','SGD'))
-    input_size = st.sidebar.selectbox('Tamaño de la entrada',(24, 48, 72, 96))
+    neurons = st.sidebar.selectbox('Número de Neuronas',(50,100,150))
+    act_func = st.sidebar.selectbox('Función de Activación',('relu','elu','LeakyReLU'))
+    loss = st.sidebar.selectbox('Función de Pérdida',('MSE','huber_loss','logcosh'))
+    optmz = st.sidebar.selectbox('Optimizador',('Adam','RMSprop'))
+    input_size = st.sidebar.selectbox('Tamaño de la entrada',(24, 48, 72))
     query = {'layer1': neurons,
             'act_func': act_func,
             'loss': loss,
@@ -60,32 +53,51 @@ def model_parameters():
             'past_hist':input_size}
     features = pd.DataFrame(query, index=[0])
     return query, features
-
+#----------------------------------------------------------------------------
 
 query, features= model_parameters()
 st.subheader('Parámetros del Modelo')
 st.write(features)
+#----------------------------------------------------------------------------
 
 st.sidebar.header('Sobre la predicción')
 min_date = dt.datetime.strptime('2019-06-03', '%Y-%m-%d')
 # date = st.sidebar.date_input('Día a predecir', data.index.get_level_values(0)[0])
 date = st.sidebar.date_input('Día a predecir', min_date,min_value=min_date)
 time = st.sidebar.time_input('A partir de las', dt.time(00, 00))
+if time.minute !=0:
+	time= dt.time(time.hour, 00)
 n_ahead = st.sidebar.slider('Horas a predecir', 1, 24, 1)
 datetime_str = str(date)+' '+str(time)
+st.info("Se realizará un a predicción de {} hora(s) del día {} a partir de las {}".format(n_ahead, date, time))
 
+#----------------------------------------------------------------------------
 
 pressed = st.button('Buscar Modelo & Predecir')
 if pressed:
+
+	load_state.warning('Cargando datos y modelos...')
+	if upload_file is not None:
+		data = load_data(upload_file)
+	else:
+		data = load_data()
+
+	#Validar entrada de fecha con la data#
+
+	data_master = data
+	list_config, list_model = load_models()
+
+	load_state.success('Carga completada!')
+#----------------------------------------------------------------------------
 	model, conf = app_tools.find_model(list_config, list_model, query)
 	if type(model)==list:
-	    st.text("Modelo no Encontrado")
+	    st.error("No se encontró un modelo con estas características en el banco de modelos")
 	    flag_input_show = False
 	else:
-	    st.text("Cargado Correctamente")
+	    st.success("Modelo cargado correctamente")
 	    flag_input_show = True
 	    data, data_r, data_mean_2, data_std_2= app_tools.get_data_2_predict(data,conf, datetime_str)
-	    st.subheader('Datos de Entrada')
+	    st.subheader('Datos seleccionados para la entrada del modelo')
 	    left_column, right_column = st.beta_columns(2)
 	    left_column.line_chart(data['ENERGY'])
 	    right_column.dataframe(data['ENERGY'])
@@ -100,8 +112,16 @@ if pressed:
 	    left_column_2.dataframe(comp[-n_ahead:])
 	    stats_df = app_tools.comp_stats(comp_df)
 	    right_column_2.dataframe(stats_df.T)
-
+	    st.markdown('***Gráfico de Predicción***')
 	    st.line_chart(comp)
+		
+	    st.vega_lite_chart(comp[-n_ahead:], {
+	    'mark': {'type': 'circle', 'tooltip': True},
+	    'encoding': {
+	        'x': {'field': 'Predicción', 'type': 'quantitative'},
+	        'y': {'field': 'Real', 'type': 'quantitative'},
+	    },
+	})
 
 # st.line_chart(data[start_date:end_date]['WS1'])
 # st.line_chart(data[start_date:end_date]['IRRAD1'])
--- univariate_2app.py
+++ univariate_2app.py
@@ -31,48 +31,62 @@ data[conf["y_var"]].astype(float)
 data_u = data[conf["y_var"]].values
 
 data_u, data_mean, data_std = ml_tools.normalize(data_u)
-
-#Predecir los N siguientes valores
-
+#-------------------------------------------------------
+err_index =[]
+err_com = []
+count=13
+cont_comb_50 = settings.comb_50[14:]
+for change in cont_comb_50:
 #Aqui comenzaria el for para generar modelos
-#-----------------------------------------------
-u_past_hist= conf["past_hist"]
-u_future_traget = conf["future_target"]
-
-train_split= ml_tools.data_split(data_u, 100)
-
-x_train, y_train = ml_tools.univariate_data(data_u, 0, train_split, u_past_hist, u_future_traget)
-
-#-----------------------------------------------
-
-
-model, m_perf = model_mk.model_maker(conf, x_train, y_train)
-model.save(settings.m_path+conf['type']+'_u'+'.h5')
-graphs.plot_model_metric(m_perf, 'loss', save = False)
-#
-#
-ml_tools.save_model_2app(conf)
-
-#Aqui termina el for para generar modelos
-
-
-#-----------------------------------------------
-n_ahead = conf["n_ahead"]
-last_input= x_train[-1]
-#-----------------------------------------------
-
-
-yhat = ml_tools.predict_n_ahead(model, n_ahead, last_input)
-
-yhat = ml_tools.desnormalize(np.array(yhat), data_mean, data_std)
-
-yhat = ml_tools.model_out_tunep(yhat)
-
-#-----------------------------------------------
-
-graphs.plot_next_forecast(data, yhat, n_ahead, save=True)
-
-fc = ml_tools.forecast_dataframe(data, yhat, n_ahead)
-fc =fc.iloc[-49:]
-print(fc)
+    count+=1
+    conf["layer1"]=change[0]
+    conf["act_func"]=change[1]
+    conf["past_hist"]=change[2]
+    conf["loss"]=change[3]
+    conf["optimizer"]=change[4]
+    with open(settings.conf_u_path, 'w') as json_file:
+        json.dump(conf, json_file, indent=4)
+    #-----------------------------------------------
+    u_past_hist= conf["past_hist"]
+    u_future_traget = conf["future_target"]
+    
+    train_split= ml_tools.data_split(data_u, 100)
+    
+    x_train, y_train = ml_tools.univariate_data(data_u, 0, train_split, u_past_hist, u_future_traget)
+    
+    #-----------------------------------------------
+    
+    
+    model, m_perf = model_mk.model_maker(conf, x_train, y_train)
+    model.save(settings.m_path+conf['type']+'_u'+'.h5')
+    graphs.plot_model_metric(m_perf, 'loss', save = False)
+    #
+    #
+    ml_tools.save_model_2app(conf)
+    
+    #Aqui termina el for para generar modelos
+    
+    
+    #-----------------------------------------------
+    n_ahead = conf["n_ahead"]
+    last_input= x_train[-1]
+    #-----------------------------------------------
+    
+    
+    yhat = ml_tools.predict_n_ahead(model, n_ahead, last_input)
+    
+    yhat = ml_tools.desnormalize(np.array(yhat), data_mean, data_std)
+    
+    yhat = ml_tools.model_out_tunep(yhat)
+    
+    #-----------------------------------------------
+    
+    graphs.plot_next_forecast(data, yhat, n_ahead, save=True)
+    
+    fc = ml_tools.forecast_dataframe(data, yhat, n_ahead)
+    fc =fc.iloc[-49:]
+    if fc["ENERGY"].values[-24:].sum() < 1000:
+        err_index.append(count-1)
+        err_com.append(change)
+    print(fc)
 
